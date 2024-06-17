966c6a12c5ec96d77e5f910ef840f1a43792642b
cristian-israel <israel.dev@icloud.com>
2024-06-17 18:28:50 -0300

    chore: Update Lucide icons in sidebar and improve code structure

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,5 +1,6 @@
 import PropTypes from "prop-types";
-import { ChevronLast, ChevronFirst, LogOut } from "lucide-react"
+
+import { MoreVertical, ChevronLast, ChevronFirst } from "lucide-react"
 import { useContext, createContext, useState } from "react"
 
 const SidebarContext = createContext()
@@ -9,7 +10,7 @@ export default function Sidebar({ children }) {
 
   return (
     <aside className="h-screen">
-      <nav className="h-full flex flex-col bg-slate-900 border-r shadow-sm">
+      <nav className="h-full flex flex-col bg-slate-50 border-r shadow-sm">
         <div className="p-4 pb-2 flex justify-between items-center">
           <img
             src="https://img.logoipsum.com/243.svg"
@@ -19,7 +20,7 @@ export default function Sidebar({ children }) {
           />
           <button
             onClick={() => setExpanded((curr) => !curr)}
-            className="p-1.5 duration-300 rounded-lg bg-slate-700 hover:bg-slate-200 hover:text-slate-700"
+            className="p-1.5 rounded-lg bg-gray-50 hover:bg-gray-100"
           >
             {expanded ? <ChevronFirst /> : <ChevronLast />}
           </button>
@@ -30,17 +31,23 @@ export default function Sidebar({ children }) {
         </SidebarContext.Provider>
 
         <div className="border-t flex p-3">
+          <img
+            src="https://ui-avatars.com/api/?background=c7d2fe&color=3730a3&bold=true"
+            alt=""
+            className="w-10 h-10 rounded-md"
+          />
           <div
             className={`
               flex justify-between items-center
               overflow-hidden transition-all ${expanded ? "w-52 ml-3" : "w-0"}
-          `}>
+          `}
+          >
+            <div className="leading-4">
+              <h4 className="font-semibold">John Doe</h4>
+              <span className="text-xs text-gray-600">johndoe@gmail.com</span>
+            </div>
+            <MoreVertical size={20} />
           </div>
-
-          <div className="leading-4">
-            <LogOut size={20} />
-          </div>
-
         </div>
       </nav>
     </aside>
@@ -48,8 +55,8 @@ export default function Sidebar({ children }) {
 }
 
 Sidebar.propTypes = {
-  children: PropTypes.node.isRequired,
-};
+  children: PropTypes.node,
+}
 
 export function SidebarItem({ icon, text, active, alert }) {
   const { expanded } = useContext(SidebarContext)
@@ -61,8 +68,8 @@ export function SidebarItem({ icon, text, active, alert }) {
         font-medium rounded-md cursor-pointer
         transition-colors group
         ${active
-          ? "bg-gradient-to-tr from-slate-200 to-slate-300 text-slate-800"
-          : "hover:bg-slate-500 text-slate-600"
+          ? "bg-gradient-to-tr from-indigo-200 to-indigo-100 text-indigo-800"
+          : "hover:bg-indigo-50 text-gray-600"
         }
     `}
     >
@@ -75,7 +82,7 @@ export function SidebarItem({ icon, text, active, alert }) {
       </span>
       {alert && (
         <div
-          className={`absolute right-2 w-2 h-2 rounded bg-slate-400 ${expanded ? "" : "top-2"
+          className={`absolute right-2 w-2 h-2 rounded bg-indigo-400 ${expanded ? "" : "top-2"
             }`}
         />
       )}
@@ -84,7 +91,7 @@ export function SidebarItem({ icon, text, active, alert }) {
         <div
           className={`
           absolute left-full rounded-md px-2 py-1 ml-6
-          bg-slate-100 text-slate-800 text-sm
+          bg-indigo-100 text-indigo-800 text-sm
           invisible opacity-20 -translate-x-3 transition-all
           group-hover:visible group-hover:opacity-100 group-hover:translate-x-0
       `}
@@ -97,8 +104,8 @@ export function SidebarItem({ icon, text, active, alert }) {
 }
 
 SidebarItem.propTypes = {
-  icon: PropTypes.node.isRequired,
-  text: PropTypes.string.isRequired,
-  active: PropTypes.bool.isRequired,
-  alert: PropTypes.bool.isRequired,
-};
+  icon: PropTypes.node,
+  text: PropTypes.string,
+  active: PropTypes.bool,
+  alert: PropTypes.bool,
+}
